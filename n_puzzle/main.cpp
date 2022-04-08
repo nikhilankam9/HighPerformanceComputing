4f4b3cedf072927b0f7c19e707d38113c8dc7739
Nikhil Ankam <nikhilgdk@gmail.com>
2022-04-08 18:12:56 -0500

    sequential branch and bound

--- n_puzzle/main.cpp
+++ n_puzzle/main.cpp
@@ -8,14 +8,18 @@ using namespace std::chrono;
 int dir_x[4] = { 1, 0, -1, 0 };
 int dir_y[4] = { 0, -1, 0, 1 };
 
-
 class Node {
     public:
         bool game_over();
-        void fill();
+        void fill_random();
+        void fill_shuffle(int s);
         void print();
-        void calculate();
+        void calculate_distance();
+        void calculate_misplaced();
         string hash();
+
+        int find_X_position();
+        bool is_solvable();
 
         Node(int b[N][N]);
 
@@ -58,7 +62,7 @@ bool Node::game_over(){
     return true;
 }
 
-void Node::calculate(){
+void Node::calculate_distance(){
     heuristic = 0;
     for (int i=0; i< N;i++){
         for(int j=0;j< N;j++){
@@ -66,72 +70,45 @@ void Node::calculate(){
             if (val == 0){
                 continue;
             }
-            // cout<<val<<"= ";
-            int x;
+            int x = val/N;
             if (val % N == 0){
-                x = val /N - 1;
-            }else{
-                x = val/N;
+                x = x - 1;
             }
             int y = val - x * N - 1;
-            int d= abs(x - i) + abs(y - j);
-            // cout<<d<<endl;
-            heuristic += d;
+            heuristic += abs(x - i) + abs(y - j);
         }
     }
-
-    // for (int i = 0; i < N; i++)
-    //   for (int j = 0; j < N; j++)
-    //     if (board[i][j] && board[i][j] != i * N + j + 1)
-    //        heuristic++;
 }
 
-void Node::fill(){
-    // vector<int> cells;
-    // for (int i = 1; i < N*N; i++){
-    //     cells.push_back(i);
-    // }
-    // auto rng = std::default_random_engine {};
-    // std::shuffle(std::begin(cells), std::end(cells), rng);
-    // for (int i = 0; i< cells.size();i++){
-    //     cout<<cells[i]<<" ";
-    // }
-    // cout<<endl;
-    // for (int i=0; i< N;i++){
-    //     for(int j=0;j< N;j++){
-    //         if (i == N-1 && j == N-1){
-    //             continue;
-    //         }
-    //         int rand_ind = rand() % cells.size();
-    //         board[i][j] = cells[rand_ind];
-    //         cells.erase(cells.begin() + rand_ind);
-    //     }
-    // }
+void Node::calculate_misplaced(){
+    heuristic = 0;
+    for (int i = 0; i < N; i++)
+      for (int j = 0; j < N; j++)
+        if (board[i][j] && board[i][j] != i * N + j + 1)
+           heuristic++;
+}
 
-
-    for (int i=0; i< N;i++){
-        for(int j=0;j< N;j++){
+void Node::fill_shuffle(int shuffles){
+    for (int i=0; i< N; i++){
+        for(int j=0; j< N; j++){
             if (i == N-1 && j == N-1){
                 continue;
             }
             board[i][j] = i * N + j + 1;
         }
     }
-
-    // print();
-
     empty_x = empty_y = N-1;
-    for (int i = 0;i<500;i++){
+    for (int i = 0; i<shuffles; i++){
         int rand_ind = rand() % 4;
         int x = empty_x + dir_x[rand_ind];
         int y = empty_y + dir_y[rand_ind];
 
         if (x < 0 || x >= N){
-                continue;
-            }
-            if (y < 0 || y >= N){
-                continue;
-            }
+            continue;
+        }
+        if (y < 0 || y >= N){
+            continue;
+        }
 
         board[empty_x][empty_y] = board[x][y];
         board[x][y] = 0;
@@ -141,58 +118,53 @@ void Node::fill(){
     }
 }
 
+void Node::fill_random(){
+    vector<int> cells;
+    for (int i = 1; i < N*N; i++){
+        cells.push_back(i);
+    }
+    auto rng = std::default_random_engine {};
+    std::shuffle(std::begin(cells), std::end(cells), rng);
+    for (int i=0; i< N;i++){
+        for(int j=0;j< N;j++){
+            if (i == N-1 && j == N-1){
+                continue;
+            }
+            int rand_ind = rand() % cells.size();
+            board[i][j] = cells[rand_ind];
+            cells.erase(cells.begin() + rand_ind);
+        }
+    }
+}
+
 void Node::print(){
     int r;
-    cout<<heuristic<< " "<<moves<<endl;
-    for( int y = 0; y < N; y++ ) {
+    // cout<<heuristic<< " "<<moves<<endl;
+    for(int i = 0; i < N; i++) {
         cout << "+----+----+----+----+\n";
-        for( int x = 0; x < N; x++ ) {
-            r = board[y][x];
+        for( int j = 0; j < N; j++) {
+            r = board[i][j];
             cout << "| ";
-            if( r < 10 ) std::cout << " ";
-            if( !r ) std::cout << "  ";
-            else std::cout << r << " ";
+            if(r < 10) cout << " ";
+            if(!r) cout << "  ";
+            else cout << r << " ";
         }
         cout << "|\n";
     }
     cout << "+----+----+----+----+\n";
-    // for (int i = 0; i < N; i++){
-    //     for (int j = 0; j <= N* 4; j++){
-    //         cout<<"-";
-    //     }
-    //     cout<<endl;
-    //     for (int j = 0; j < N; j++){
-    //         cout<<"| ";
-    //         if (board[i][j] < 10){
-    //             cout <<" ";
-    //         }
-    //         if (board[i][j] == 0){
-    //             cout << " ";
-    //         }else{
-    //             cout<<board[i][j];
-    //         }
-    //     }
-    //     cout<<"|"<<endl;
-    // }
-    // for (int j = 0; j <= N* 4; j++){
-    //     cout<<"-";
-    // }
-    // cout<<endl;
 }
-
-
 
 struct compare{
     public:
     bool operator()(Node a,Node b) {
-        // if (a.heuristic + a.moves == b.heuristic + b.moves){
-        //     return a.moves < b.moves;
-        // }
+        if (a.heuristic + a.moves == b.heuristic + b.moves){
+            return a.moves < b.moves;
+        }
         return a.heuristic + a.moves > b.heuristic + b.moves; 
     }
 };
 
-int getInvCount(int arr[]){
+int get_inv_count(int arr[]){
     int inv_count = 0;
     for (int i = 0; i < N * N - 1; i++){
         for (int j = i + 1; j < N * N; j++){
@@ -203,10 +175,10 @@ int getInvCount(int arr[]){
     return inv_count;
 }
  
-int findXPosition(int puzzle[N][N]){
+int Node::find_X_position(){
     for (int i = N - 1; i >= 0; i--){
         for (int j = N - 1; j >= 0; j--){
-            if (puzzle[i][j] == 0){
+            if (board[i][j] == 0){
                 return N - i;
             }
         }
@@ -214,12 +186,12 @@ int findXPosition(int puzzle[N][N]){
     return 0;
 }
 
-bool isSolvable(int puzzle[N][N]){
-    int invCount = getInvCount((int*)puzzle);
+bool Node::is_solvable(){
+    int invCount = get_inv_count((int*)board);
     if (N & 1)
         return !(invCount & 1);
     else{
-        int pos = findXPosition(puzzle);
+        int pos = find_X_position();
         if (pos & 1)
             return !(invCount & 1);
         else
@@ -233,11 +205,11 @@ int main(){
     Node root(board);
     // root.empty_x = 1;
     // root.empty_y = 2;
-    root.fill();
-    root.calculate();
-    if (isSolvable(root.board)){
-    root.print();
-        cout<<"Solvable\n";
+    root.fill_shuffle(500);
+    root.calculate_distance();
+    if (root.is_solvable()){
+        root.print();
+        cout<<"*****************Solvable*****************\n";
         sleep(1);
     }
 
@@ -249,22 +221,18 @@ int main(){
 
     auto start = high_resolution_clock::now();
 
-
     while (q.size() > 0){
         Node curr = q.top();
         q.pop();
-        // curr.print(); 
 
         if (curr.game_over() == true){
-            
+            curr.print();
             break;
         }
-
 
         for (int d = 0; d <4; d++){
             int x = curr.empty_x + dir_x[d];
             int y = curr.empty_y + dir_y[d];
-
 
             if (x < 0 || x >= N){
                 continue;
@@ -272,7 +240,6 @@ int main(){
             if (y < 0 || y >= N){
                 continue;
             }
-            // cout<<curr.empty_x<<" "<<curr.empty_y<<" - "<<x<<" "<<y<<endl;
 
             Node next(curr.board);
             next.board[curr.empty_x][curr.empty_y] = curr.board[x][y];
@@ -280,7 +247,7 @@ int main(){
             next.empty_x = x;
             next.empty_y = y;
             next.moves = curr.moves +1;
-            next.calculate();
+            next.calculate_distance();
             if (mp[next.hash()] == 0){
                 mp[next.hash()] = 1;
                 q.push(next);
